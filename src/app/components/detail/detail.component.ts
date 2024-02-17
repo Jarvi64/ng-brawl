29da0d5a2d01ccd4c215356c2aacdb09dffc66d4
Jarvi64 <129019096+Jarvi64@users.noreply.github.com>
2024-02-17 16:23:39 +0530

    Add files via upload

---
+++ src/app/components/detail/detail.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlayerDto } from '../../models/player';
+
+@Component({
+  selector: 'app-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.css'
+})
+export class DetailComponent implements OnInit{
+  tag?:string;
+  http = inject(HttpClient);
+  player?:PlayerDto;
+  victory?:number;
+  ngOnInit() {
+    this.tag = this.activateRoute.snapshot.params['tag'];
+    this.http.get<any>('https://brawl-gang-backend.onrender.com/player/'+ this.tag?.substring(1)).subscribe(
+      {
+        next:(res)=>{this.player = res;
+           this.victory = res['3vs3Victories'];
+        },
+        error:(err)=>{console.log(err);
+        }
+      }
+    )
+  }
+  activateRoute = inject(ActivatedRoute);
+}
