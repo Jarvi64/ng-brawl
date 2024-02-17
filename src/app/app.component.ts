9e385bcac354028b88a8237ab7afb03077306448
Jarvi64 <129019096+Jarvi64@users.noreply.github.com>
2024-02-17 16:25:51 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component} from '@angular/core';
+import { Component, effect, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
+import { getState } from '@ngrx/signals';
+import { BrawlStore } from './store/brawl.store';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,16 @@ import { HeroComponent } from './components/hero/hero.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent  {
-  
+  readonly store = inject(BrawlStore);
+
+  constructor() {
+    effect(() => {
+      const state = getState(this.store);
+      console.log('brawl state changed', state);
+    });
+  }
+
+  ngOnInit(): void {
+    this.store.loadClubInfo();
+  }
 }
