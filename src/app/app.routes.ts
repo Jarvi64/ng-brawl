d4377d6557740007ea6ebe1f06572945cfb0d763
Jarvi64 <129019096+Jarvi64@users.noreply.github.com>
2024-02-17 16:26:22 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { DetailComponent } from './components/detail/detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'gang' ,pathMatch: 'full'},
+    { path: 'gang', component: HeroComponent },
+    { path: 'gang/:tag', component: DetailComponent }
+];
